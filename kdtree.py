06f6c402243cea7b44a51efe19f16e65833fe268
xiangrmz <xiang.zhang@intel.com>
2022-12-01 00:04:41 -0800

    add generate dataset scripts

--- kdtree.py
+++ kdtree.py
@@ -6,7 +6,7 @@ patch_sklearn()
 from sklearn.neighbors import KNeighborsClassifier
 
 
-num_thread = 112
+num_thread = 72
 
 if len(sys.argv) == 2:
     num_thread = int(sys.argv[1])
@@ -22,14 +22,11 @@ def set_daal_num_threads(num_threads):
 train_file_x = "bigdata/synthetic-classification-10-X-train-250000x16_256_tile.npy"
 train_file_y = "bigdata/synthetic-classification-10-y-train-250000x16_256_tile.npy"
 
-train_file_x = "data/synthetic-classification-10-X-train-16000000x128.npy"
-train_file_y = "data/synthetic-classification-10-y-train-16000000x128.npy"
+train_file_x = "synthetic-classification-10-X-train-16000000x16.npy"
+train_file_y = "synthetic-classification-10-y-train-16000000x16.npy"
 
-# train_file_x = "data/synthetic-classification-10-X-train-16000000x16.npy"
-# train_file_y = "data/synthetic-classification-10-y-train-16000000x16.npy"
-
-# train_file_x = "data/synthetic-classification-10-X-train-128000000x16.npy"
-# train_file_y = "data/synthetic-classification-10-y-train-128000000x16.npy"
+# train_file_x = "bigdata/synthetic-classification-10-X-train-250000x16_8_tile.npy"
+# train_file_y = "bigdata/synthetic-classification-10-y-train-250000x16_8_tile.npy"
 
 # train_file_x = "bigdata/synthetic-classification-10-X-train-2000000x16_8_tile_col.npy"
 # train_file_y = "bigdata/synthetic-classification-10-y-train-2000000x16_8_tile_col.npy"
@@ -37,18 +34,26 @@ train_file_y = "data/synthetic-classification-10-y-train-16000000x128.npy"
 # train_file_x = "bigdata/synthetic-classification-10-X-train-16000000x16_8_tile_col.npy"
 # train_file_y = "bigdata/synthetic-classification-10-y-train-16000000x16_8_tile_col.npy"
 
-# train_file_x = "synthetic-classification-10-X-train-250000x16.npy"
-# train_file_y = "synthetic-classification-10-y-train-250000x16.npy"
+train_file_x = "data/synthetic-classification-10-X-train-250000x16.npy"
+train_file_y = "data/synthetic-classification-10-y-train-250000x16.npy"
+test_file_x  = "data/synthetic-classification-10-X-train-250000x16.npy"
+test_file_y  = "data/synthetic-classification-10-y-train-250000x16.npy"
+X_test = np.load(test_file_x)
+y_test = np.load(test_file_y)
 
 data_order = "F"
 set_daal_num_threads(num_thread)
 
 X_train = np.load(train_file_x)
 y_train = np.load(train_file_y)
-
+print(type(X_train), np.isfortran(X_train), isinstance(X_train, np.ndarray))
+print(type(y_train))
 if data_order == 'F':
     X_train = np.asfortranarray(X_train, float)
     y_train = np.asfortranarray(y_train, float)
+    print(type(X_train), np.isfortran(X_train), isinstance(X_train, np.ndarray))
+    print(type(y_train), isinstance(np.asarray([1]), np.ndarray))
+
 elif data_order == 'C':
     X_train = np.ascontiguousarray(X_train, float)
     y_train = np.ascontiguousarray(y_train, float)
@@ -57,6 +62,8 @@ knn_clsf = KNeighborsClassifier(n_neighbors=10,
                                 weights='uniform',
                                 algorithm='kd_tree',
                                 metric='euclidean')
+print(y_train, y_train.shape)
+# exit()
 start = time.time()
 print("start")
 print(start)
@@ -70,4 +77,9 @@ names = train_file_x.partition("-train-")
 print(names[0])
 print(names[-1])
 # print(knn_clsf.predict(X_train),'\n', y_train)
-print(knn_clsf)
+print("accuracy")
+y_pred = knn_clsf.predict(X_test)
+print((y_test == y_pred).sum()/250000)
+
+print(y_test)
+print(y_pred)
