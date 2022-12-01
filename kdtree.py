eef2eba0a4da81d961be4ec7a0f8ca9026ac8020
xiangrmz <xiang.zhang@intel.com>
2022-12-01 01:43:36 -0800

    ready dataset added: for final testing

--- kdtree.py
+++ kdtree.py
@@ -11,6 +11,7 @@ num_thread = 72
 if len(sys.argv) == 2:
     num_thread = int(sys.argv[1])
 
+
 def set_daal_num_threads(num_threads):
     try:
         import daal4py
@@ -19,23 +20,41 @@ def set_daal_num_threads(num_threads):
     except ImportError:
         raise('@ Package "daal4py" was not found. Number of threads '
                      'is being ignored')
-train_file_x = "bigdata/synthetic-classification-10-X-train-250000x16_256_tile.npy"
-train_file_y = "bigdata/synthetic-classification-10-y-train-250000x16_256_tile.npy"
+train_file_x = "synthetic-classification-10-X-train-250000x16"
+train_file_y = "synthetic-classification-10-y-train-250000x16"
+
+train_file_x = "synthetic-classification-10-X-train-2000000x16.npy"
+train_file_y = "synthetic-classification-10-y-train-2000000x16.npy"
 
 train_file_x = "synthetic-classification-10-X-train-16000000x16.npy"
 train_file_y = "synthetic-classification-10-y-train-16000000x16.npy"
 
-# train_file_x = "bigdata/synthetic-classification-10-X-train-250000x16_8_tile.npy"
-# train_file_y = "bigdata/synthetic-classification-10-y-train-250000x16_8_tile.npy"
+train_file_x = "synthetic-classification-10-X-train-64000000x16.npy"
+train_file_y = "synthetic-classification-10-y-train-64000000x16.npy"
 
-# train_file_x = "bigdata/synthetic-classification-10-X-train-2000000x16_8_tile_col.npy"
-# train_file_y = "bigdata/synthetic-classification-10-y-train-2000000x16_8_tile_col.npy"
+train_file_x = "synthetic-classification-10-X-train-128000000x16.npy"
+train_file_y = "synthetic-classification-10-y-train-128000000x16.npy"
 
-# train_file_x = "bigdata/synthetic-classification-10-X-train-16000000x16_8_tile_col.npy"
-# train_file_y = "bigdata/synthetic-classification-10-y-train-16000000x16_8_tile_col.npy"
+train_file_x = "synthetic-classification-10-X-train-256000000x16.npy"
+train_file_y = "synthetic-classification-10-y-train-256000000x16.npy"
 
-train_file_x = "data/synthetic-classification-10-X-train-250000x16.npy"
-train_file_y = "data/synthetic-classification-10-y-train-250000x16.npy"
+train_file_x = "synthetic-classification-2-X-train-100000x3.npy"
+train_file_y = "synthetic-classification-2-y-train-100000x3.npy"
+
+train_file_x = "synthetic-classification-2-X-train-2000000x3.npy"
+train_file_y = "synthetic-classification-2-y-train-2000000x3.npy"
+
+train_file_x = "synthetic-classification-2-X-train-16000000x3.npy"
+train_file_y = "synthetic-classification-2-y-train-16000000x3.npy"
+
+train_file_x = "synthetic-classification-2-X-train-64000000x3.npy"
+train_file_y = "synthetic-classification-2-y-train-64000000x3.npy"
+
+train_file_x = "synthetic-classification-2-X-train-128000000x3.npy"
+train_file_y = "synthetic-classification-2-y-train-128000000x3.npy"
+
+data_floder = "data/"
+
 test_file_x  = "data/synthetic-classification-10-X-train-250000x16.npy"
 test_file_y  = "data/synthetic-classification-10-y-train-250000x16.npy"
 X_test = np.load(test_file_x)
@@ -44,15 +63,13 @@ y_test = np.load(test_file_y)
 data_order = "F"
 set_daal_num_threads(num_thread)
 
-X_train = np.load(train_file_x)
-y_train = np.load(train_file_y)
-print(type(X_train), np.isfortran(X_train), isinstance(X_train, np.ndarray))
-print(type(y_train))
+X_train = np.load(data_floder + train_file_x)
+y_train = np.load(data_floder + train_file_y)
+
+
 if data_order == 'F':
     X_train = np.asfortranarray(X_train, float)
     y_train = np.asfortranarray(y_train, float)
-    print(type(X_train), np.isfortran(X_train), isinstance(X_train, np.ndarray))
-    print(type(y_train), isinstance(np.asarray([1]), np.ndarray))
 
 elif data_order == 'C':
     X_train = np.ascontiguousarray(X_train, float)
@@ -62,8 +79,7 @@ knn_clsf = KNeighborsClassifier(n_neighbors=10,
                                 weights='uniform',
                                 algorithm='kd_tree',
                                 metric='euclidean')
-print(y_train, y_train.shape)
-# exit()
+
 start = time.time()
 print("start")
 print(start)
